e7ce8aa22feec6902db317a4cdb52551dc4335c2
hejianglong <hejlong@163.com>
2019-03-07 20:51:48 +0800

    配置 API配置

--- consumer/src/main/java/com/dubbos/consumer/Consumer.java
+++ consumer/src/main/java/com/dubbos/consumer/Consumer.java
@@ -1,6 +1,9 @@
 package com.dubbos.consumer;
 
 import com.dubbos.api.DemoService;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
@@ -8,10 +11,18 @@ import java.io.IOException;
 public class Consumer {
 
     public static void main(String[] args) throws IOException {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:consumer.xml");
-        applicationContext.start();
-        DemoService demoService = (DemoService) applicationContext.getBean("demoService");
-        String hello = demoService.sayHello("along");
-        System.out.println(hello);
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("yyy");
+
+        RegistryConfig registry = new RegistryConfig();
+        registry.setAddress("zookeeper://127.0.0.1:2181");
+
+        ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
+        reference.setApplication(application);
+        reference.setRegistry(registry);
+        reference.setInterface(DemoService.class);
+        reference.setVersion("1.0.0");
+        DemoService demoService = reference.get();
+        System.out.println(demoService.sayHello("pangge"));
     }
 }
--- provider/src/main/java/com/dubbos/provider/Provider.java
+++ provider/src/main/java/com/dubbos/provider/Provider.java
@@ -1,13 +1,43 @@
 package com.dubbos.provider;
 
+import com.dubbos.api.DemoService;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ProtocolConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.ServiceConfig;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.io.IOException;
 
 public class Provider {
 
     public static void main(String[] args) throws IOException {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:provider.xml");
-        applicationContext.start();
+        DemoService demoService = new DemoServiceImpl();
+
+        // 当前应用配置
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName("provider");
+
+        // 连接注册中心配置
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setAddress("zookeeper://127.0.0.1:2181");
+
+        // 服务提供者协议配置
+        ProtocolConfig protocol = new ProtocolConfig();
+        protocol.setName("dubbo");
+        protocol.setPort(12345);
+        protocol.setThreads(200);
+
+        // 服务提供者暴露服务配置
+        ServiceConfig<DemoService> service = new ServiceConfig<>();
+        service.setApplication(applicationConfig);
+        service.setRegistry(registryConfig);
+        service.setProtocol(protocol);
+        service.setInterface(DemoService.class);
+        service.setRef(demoService);
+        service.setVersion("1.0.0");
+
+        service.export();
         System.in.read();
+
     }
 }
